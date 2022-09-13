97b50a8d03a1f2dbc7635c9317e639b1159b6a05
rahimwebdeveloper <info.abdurrahim12@gmail.com>
2022-09-13 11:12:36 +0600

    done

--- Index.js
+++ Index.js
@@ -13,10 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e5xjxv3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
